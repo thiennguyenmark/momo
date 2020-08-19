9d8a9864bf27a6064a1ce2025a6c945ce53ee2b0
ITadmin <itadmin@ITadmins-MacBook-Pro.local>
2020-08-19 15:36:14 +0700

    Fix Naming

--- lib/momo_pay/http.rb
+++ lib/momo_pay/http.rb
@@ -2,7 +2,7 @@ module MomoPay
   class Http
 
     def self.post(*args)
-      ::Http.headers(content_type: 'application/json; charset=UTF-8')
+      ::HTTP.headers(content_type: 'application/json; charset=UTF-8')
         .timeout(60)
         .post(*args)
     end
